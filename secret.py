985c2ab6cfbac23189869563f78902ece8add01a
anilrathod11 <118740702+anilrathod11@users.noreply.github.com>
2026-01-16 13:39:22 +0530

    Update secret.py

--- secret.py
+++ secret.py
@@ -1,2 +1,2 @@
 github_token = "ghp_zVuw8MJm2lIs0IuK66JoSXFQ72345rt"
-key = "sk-proj-30aUZFVfvDZ1vP6Dv_9y3Wwsedrtyhjf;gdlh"
+key = "sk-proj-30aUZFVfvDZ1vP6Dv_9y3Wwsedrtyhjf;gdlhwaerhjgkhjlewrthjk;ilkjfdhsytwterhgj"
