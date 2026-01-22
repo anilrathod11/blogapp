8d5211d447aa52af8f7cb93ed72a8d21760dcf9d
anilrathod11 <118740702+anilrathod11@users.noreply.github.com>
2026-01-22 17:04:36 +0530

    Update secret.py

--- secret.py
+++ secret.py
@@ -3,3 +3,9 @@ key = "sk-proj-30aUZFVfvDZ1vP6Dv_9y3Wwsedrtyhjf;gdlhwaerhjgkhjlewrthjk;ilkjfdhsy
 AUTOGEN_MODEL_TYPE="openai"
 DEVOPS_MCP_SERVER_URL="http://127.0.0.1:8012/devops/mcp"
 SECSCAN_MCP_SERVER_URL="http://127.0.0.1:8080/secops/mcp"
+TENANT_ID ="2da40318-46be-402c-ba75-cfb1f656567d",
+CLIENT_ID ="611362bb-973e-4f76-9521-8e799488729d",
+REDACTED_SECRET
+sonar_qube_token = "squ_04d9fe15f80cbbeee316bbf0c0ad5d7997657336"
+jerkins_token = "11b8cb112292f87d6a9eee6d4933bff33c"
+IAM_user_secret_key =  "ZEqCS4GqM3ep5TZZOhWYiFM7GSVXgXXsEO7EzseE"
