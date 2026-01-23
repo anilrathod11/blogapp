348d5b5e06301716de430a3c0cadb5e034d7a8df
anilrathod11 <118740702+anilrathod11@users.noreply.github.com>
2026-01-23 17:00:17 +0530

    Update secret.py

--- secret.py
+++ secret.py
@@ -1,11 +1,11 @@
-github_token = "ghp_zVuw8MJm2lIs0IuK66JoSXFQ72345rt"
-key = "sk-proj-30aUZFVfvDZ1vP6Dv_9y3Wwsedrtyhjf;gdlhwaerhjgkhjlewrthjk;ilkjfdhsytwterhgj"
+REDACTED_SECRET
+key = "sk-proj-30aUZFVfvDZ1vP6Dv_9y3Wwsedrtyhjfrtyuigdlhwaerhjgkhjlewrthjk;ilkjfdhsytwterhgj"
 AUTOGEN_MODEL_TYPE="openai"
 DEVOPS_MCP_SERVER_URL="http://127.0.0.1:8012/devops/mcp"
 SECSCAN_MCP_SERVER_URL="http://127.0.0.1:8080/secops/mcp"
-TENANT_ID ="2da40318-46be-402c-ba75-cfb1f656567d",
-CLIENT_ID ="611362bb-973e-4f76-9521-8e799488729d",
+TENANT_ID ="e8f919af-99a1-4b07-a962-81c872805415",
+CLIENT_ID ="a2f817b0-e9bf-4a84-84f7-b233b956f5d7",
 REDACTED_SECRET
-sonar_qube_token = "squ_04d9fe15f80cbbeee316bbf0c0ad5d7997657336"
-jerkins_token = "11b8cb112292f87d6a9eee6d4933bff33c"
-IAM_user_secret_key =  "ZEqCS4GqM3ep5TZZOhWYiFM7GSVXgXXsEO7EzseE"
+sonar_qube_token = "squ_04d9fe15f80cbbeee316bbf0c0ad523er97657336"
+jerkins_token = "11b8cb232292f87d6a9erfe6d4933bff33c"
+IAM_user_secret_key =  "GFeCS4GqM3ep5TZZOhWYiFM7GSAEFSGEXXsEO7EzseE"
